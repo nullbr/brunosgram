be3295b3546331c2505a2340d03ab03e389894e5
nullbr <bmarianoleite3@gmail.com>
2022-04-18 18:33:44 -0300

    replicated frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://i.pinimg.com/originals/62/9f/21/629f2154a091599beadf9c91ea657a70.jpg" style="height: 150px; width: 151px;" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>brmariano13</h1></div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>21</strong> Publicações</div>
+                <div class="pe-5"><strong>273</strong> Seguidores</div>
+                <div class="pe-5"><strong>502</strong> Seguindo</div>
             </div>
+            <div class="pt-4"><strong>freeCodeCamp.org</strong></div>
+            <div>
+                We're a global community of millions of people learning to code together.
+                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/
+            </div>
+            <div><strong><a href="#">www.freecodecamp.org</a></strong></div>
+        </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://res.cloudinary.com/grand-canyon-university/image/fetch/w_750,h_564,c_fill,g_faces,q_auto/https://www.gcu.edu/sites/default/files/2020-09/programming.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.pcworld.com/wp-content/uploads/2021/09/sale_22062_article_image-100893967-orig.jpg?quality=50&strip=all&w=1024" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh1Tvb7tXgw9bjWbMMReStqZ46nuvd8ET3YAKd3CQmm7FFc8o6OIopsDuQC_5G_zGAJ54&usqp=CAU" class="w-100">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/BrunosGramLogo.svg" style="height: 30px; border-right: 1px solid #333;" class="pe-3"></div>
+                    <div class="ps-3">BrunosGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
