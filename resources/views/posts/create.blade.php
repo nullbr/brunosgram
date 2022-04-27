c355182ce95b3a40d032fc267ff63e2f1c892cb8
nullbr <bmarianoleite3@gmail.com>
2022-04-27 15:36:58 -0300

    merging posts files

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
+    
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="row">
+                    <h1>Add New Post</h1>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+
+                    <input id="caption" type="text" 
+                        class="form-control @error('caption') is-invalid @enderror" 
+                        name="caption"
+                        value="{{ old('caption') }}" 
+                        required autocomplete="caption" autofocus>
+
+                    @error('caption')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+
+                    <input type="file" class="form-control-file" id="image" name="image">
+
+                    @error('image')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
