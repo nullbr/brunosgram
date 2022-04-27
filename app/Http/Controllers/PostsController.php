3372a2709b969d3bf8107bdcfd518888ed335988
nullbr <bmarianoleite3@gmail.com>
2022-04-27 15:30:56 -0300

    creating posts pages

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts.create');
@@ -18,6 +23,14 @@ class PostsController extends Controller
             'image' => ['required', 'image']
         ]);
         
-        \App\Models\Post::create($data);
+
+        $imagePath = request('image')->store('uploads', 'public');
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id); 
     }
 }
