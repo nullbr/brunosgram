9890f6b581ab31c36371ca9927e0658a9676aed9
nullbr <bmarianoleite3@gmail.com>
2022-05-04 19:36:51 -0300

    add welcome email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -47,12 +49,13 @@ class User extends Authenticatable
     {
         parent::boot();
 
-        static::created(
-            function ($user) {
+        static::created(function ($user) {
                 $user->profile()->create([
                     'title' => $user->username,
                 ]);
-            });
+
+                Mail::to($user->email)->send(new NewUserWelcomeMail());
+        });
     }
 
     /** setting up relationship with Post model **/
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::get('/email', function () {
+    return new \App\Mail\NewUserWelcomeMail();
+});
+
 Route::post('follow/{user}', [App\Http\Controllers\FollowsController::class, 'store']);
 
 Route::get('/',[App\Http\Controllers\PostsController::class, 'index']);
